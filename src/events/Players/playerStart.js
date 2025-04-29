c4fce62239dcd2bcc87cbbc923d5715e4813203c
lohit <lohitsaidev@gmail.com>
2025-04-29 14:00:55 +0530

    Chore: Change player start event

--- src/events/Players/playerStart.js
+++ src/events/Players/playerStart.js
@@ -16,16 +16,18 @@ module.exports = {
     if (!guild) return;
     let channel = guild.channels.cache.get(player.text);
     if (!channel) return;
+
+    // Handle setup channel if it exists
     let data = await db.findOne({ Guild: guild.id });
     if (data && data.Channel) {
       let textChannel = guild.channels.cache.get(data.Channel);
       const id = data.Message;
-      if (channel === textChannel) {
-        return await trackStartEventHandler(id, textChannel, player, track, client);
-      } else {
+      if (textChannel && textChannel.id) {
         await trackStartEventHandler(id, textChannel, player, track, client);
       }
     }
+
+    // Always send the now playing embed to the player's text channel
     const emojiplay = client.emoji.play;
 
     const main = new MessageEmbed()
@@ -39,10 +41,12 @@ module.exports = {
         `${track.thumbnail ? track.thumbnail : `https://img.youtube.com/vi/${player.current.identifier}/hqdefault.jpg`}`
       );
 
-    client.channels.cache
-      .get(player.text)
-      ?.send({ embeds: [main] })
-      .then(x => player.data.set('message', x));
+    // Send the embed to the player's text channel and store the message
+    channel
+      .send({ embeds: [main] })
+      .then(x => player.data.set('message', x))
+      .catch(err => console.error('Error sending player start message:', err));
+
     await player.data.set('autoplaySystem', player.current.identifier);
   },
 };
